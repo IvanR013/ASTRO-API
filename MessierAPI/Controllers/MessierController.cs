34c3a2150080eab70dbcd4710294b51e127de47a
Ivan Rodriguez. <ivanrwcm25@gmail.com>
2025-04-21 01:38:38 -0300

    improve reading

--- MessierAPI/Controllers/MessierController.cs
+++ MessierAPI/Controllers/MessierController.cs
@@ -3,44 +3,45 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 
-namespace MessierAPI.Controllers
+namespace MessierAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MessierController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class MessierController : ControllerBase
+    private readonly List<MessierModel> _messier;
+    public MessierController()
     {
-        private readonly List<MessierModel> _messier;
-        public MessierController()
-        {
-            var JsonData = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Messier.json");
-            _messier = JsonConvert.DeserializeObject<List<MessierModel>>(System.IO.File.ReadAllText(JsonData)) ?? new List<MessierModel>();
-        }
-
-//General Delivery data.
-        [HttpGet("Data")]
-        public IActionResult GetMessierObjects()
-        {
-            if (!_messier.Any())
-            {
-                return NotFound(new { message = "No se encontraron datos." });
-            }
-
-            return Ok(new { Status = "Success", Data = _messier });
-        }
-
-//This endpoint tracks with a type filter.
-        [HttpGet("{tipo}")]
-        public IActionResult GetMessierObject(string tipo)
-        {
-            var Objeto = _messier.FirstOrDefault(n => n.Tipo.ToLower() == tipo.ToLower());
-
-            if (Objeto == null)
-            {
-                return NotFound(new { message = "No se encontraron datos." });
-            }
-
-            return Ok(new { Status = "Success", Data = Objeto });
-        }
-
+        var JsonData = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Messier.json");
+        _messier = JsonConvert.DeserializeObject<List<MessierModel>>(System.IO.File.ReadAllText(JsonData)) ?? new List<MessierModel>();
     }
+
+    //General Delivery data.
+    [HttpGet("Data")]
+    public IActionResult GetMessierObjects()
+    {
+        if (!_messier.Any()) return NotFound(new { message = "No se encontraron datos." });
+
+        return Ok(new { Status = "Success", Data = _messier });
+    }
+
+
+
+
+    //This endpoint tracks with a type filter.
+    [HttpGet("{tipo}")]
+    public IActionResult GetMessierObject(string tipo)
+    {
+        MessierModel? Objeto = _messier.FirstOrDefault(n => n.Tipo.ToLower() == tipo.ToLower());
+
+        if (Objeto is null) return NotFound(new { message = "No se encontraron datos." });
+        
+        return Ok(new { Status = "Success", Data = Objeto });
+    }
+
 }
+
+
+
+
+
--- MessierAPI/Controllers/PlanetsController.cs
+++ MessierAPI/Controllers/PlanetsController.cs
@@ -2,44 +2,43 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using MessierAPI.Models;
 
-namespace MessierAPI.Controllers
+namespace MessierAPI.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlanetsController : ControllerBase
 {
+    private readonly List<PlanetsModel> _planets;
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class PlanetsController : ControllerBase
+    public PlanetsController()
     {
-        private readonly List<PlanetsModel> _planets;
-
-        public PlanetsController()
-        {
-            var JsonData = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Planets.json");
-            _planets = JsonConvert.DeserializeObject<List<PlanetsModel>>(System.IO.File.ReadAllText(JsonData)) ?? new List<PlanetsModel>();
-        }
-
-        [HttpGet("Data")]
-        public IActionResult GetPlanets()
-        {
-            if (!_planets.Any())
-            {
-                return NotFound(new { message = "No se encontraron datos." });
-            }
-
-            return Ok(new { Status = "Success", Data = _planets });
-        }
-
-        [HttpGet("{tipo}")]
-        public IActionResult GetType(string tipo)
-        {
-            var Objeto = _planets.FirstOrDefault(n => n.Tipo.ToLower() == tipo.ToLower());
-
-            if (Objeto == null)
-            {
-                return NotFound(new { message = "No se encontraron datos." });
-            }
-
-            return Ok(new { Status = "Success", Data = Objeto });
-        }
+        var JsonData = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Planets.json");
+        _planets = JsonConvert.DeserializeObject<List<PlanetsModel>>(System.IO.File.ReadAllText(JsonData)) ?? new List<PlanetsModel>();
     }
 
+    [HttpGet("Data")]
+    public IActionResult GetPlanets()
+    {
+        if (!_planets.Any()) return NotFound(new { message = "No se encontraron datos." });
+
+        return Ok(new { Status = "Success", Data = _planets });
+    }
+
+
+
+
+    [HttpGet("{tipo}")]
+    public IActionResult GetType(string tipo)
+    {
+        PlanetsModel? Objeto = _planets.FirstOrDefault(n => n.Tipo.ToLower() == tipo.ToLower());
+
+        if (Objeto is null) return NotFound(new { message = "No se encontraron datos." });
+
+        return Ok(new { Status = "Success", Data = Objeto });
+    }
 }
+
+
+
+
